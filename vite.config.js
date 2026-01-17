c3366477f7d009c630851cb27fad97192634ee1a
abhinavmabhi <dexmarketing192018@gmail.com>
2026-01-17 14:42:58 +0530

    made changes on vite config

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://jajis.up.railway.app/api/',
-    //     // target: 'http://127.0.0.1:8000/api/',
-    //     changeOrigin: true,
-    //     secure: true,
-    //     rewrite: (path) => path.replace(/^\/api/, '')
-    //   }
-    // }
+
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: 'all'
   }
 })
